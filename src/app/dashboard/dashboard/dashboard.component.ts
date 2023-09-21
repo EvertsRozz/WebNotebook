a26510b8e3ddb0e66699c5f6dcbd84e30c6cba38
Everts Rozenbaums <evertrozenbaum@gmail.com>
2023-09-21 22:11:14 +0300

    started doing calendar box. working on backend rn

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -6,11 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: [
     './dashboard.component.css',
     './css/dashboard-box-nav.css',
-    './css/dashboard-box-cards.css'
+    './css/dashboard-box-cards.css',
+    './css/calendar-box.css'
   ],
 })
 export class DashboardComponent {
   // temporary folder names while im making the front end.
   // TODO: make backend connection for folders and their names
-  folderNames:string[] = ['Math', 'Programming', 'Daily Notes', 'TODO Journal', 'Book Reviews', 'Lorem Ipsum']
+  folderNames:string[] = ['Math', 'Programming', 'Daily Notes', 'TODO Journal', 'Book Reviews', 'Lorem Ipsum'] 
 }
